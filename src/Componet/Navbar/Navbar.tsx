2f63103d333a5bbcef033297f6a26b620b11f7c1
Tejas Thorat <tejasthorat7777@gmail.com>
2024-05-06 00:21:58 +0530

    add search bar and profile icon

--- src/Componet/Navbar/Navbar.tsx
+++ src/Componet/Navbar/Navbar.tsx
@@ -1,27 +1,73 @@
-import { TextField } from "@mui/material";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Tooltip } from "@mui/material";
 
+const navStyle = {
+  display: "flex",
+  flexDirection: "column",
+  height: "8vh",
+  overflow: "hidden",
+  backgroundColor: "#00111c"
+}
 
+const inputStyle = {
+  width: "100%", 
+  height: "100%", 
+  backgroundColor: "transparent",
+  color: "white", 
+  outline: "0", 
+  border: "none"
+}
+
+const innerNav = {
+  height: "100%", 
+  width: "100%", 
+  display: "flex",
+  alignItems: "center", 
+  justifyContent: "space-between"
+}
+
+const navLeft = {
+  display: "flex",
+  alignItems: "center", 
+  height: "100%", 
+  width: "100%", 
+  paddingLeft: "1rem"
+}
 
 export default function Navbar() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const inputDivStyle = {
+    width: "15%", 
+    height: "50%",
+    borderBottom: isHovered ? "2px solid white" : "1px solid white",
+    display: "flex", 
+    alignItems: "center"
+  }
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "8vh",
-        overflow: "hidden",
-        backgroundColor: "#00111c",
-      }}
+    <div style={navStyle}
     >
-      <div style={{ height: "100%", width: "100%", display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-        <img
-          src={logo}
-          alt="LOGO"
-          style={{ height: "100%", scale: "0.8", marginLeft: "3.5%" }}
-        />
-        <div style={{ width: "15%", height: "50%", backgroundColor: "red" }}>
-          <input type="text" placeholder="Search" style={{ width: "100%", height: "100%" }} />
+      <div style={innerNav}>
+        <div
+          style={navLeft}>
+          <Tooltip title="Profile"><AccountCircleOutlinedIcon style={{ color: "#CFCFCD" }} fontSize="large" /></Tooltip>
+          {/* <ShoppingCartOutlinedIcon fontSize="medium" style={{ color: "#BEA8A7", marginLeft: "2rem" }} /> */}
+          <img
+            src={logo}
+            alt="LOGO"
+            style={{ height: "100%", scale: "0.8", marginLeft: "2rem" }}
+          />
+        </div>
+        <div
+          style={inputDivStyle}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}>
+          <input type="text" placeholder="search..."
+            style={inputStyle} />
         </div>
       </div>
     </div>
