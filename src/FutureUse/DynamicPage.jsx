96bb00cd941e4065b8fd3fe6ee9a2d698c27750b
Tejas Arun Thorat <tejasthorat7777@gmail.com>
2024-06-06 23:57:40 +0530

    changes in dynamic page

--- src/FutureUse/DynamicPage.jsx
+++ src/FutureUse/DynamicPage.jsx
@@ -2,14 +2,21 @@ import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import page_routing from "./page_routing"
 
-const DynamicPage = () => {
+const DynamicPage = ({page}) => {
+
+  const newPage = [];
+  page_routing.map((ObjectPage, i) => {
+    if (ObjectPage.url === page) {
+      newPage.push(ObjectPage);
+    }
+  });
   return (
     <div id="dynamic_page" className="container-fluid h-100">
       <Suspense fallback={<div>loading...</div>}>
         <Routes>
-          {page_routing.map((objRoute, i) => (
+          {newPage.map((objRoute, i) => (
             <Route
-              exact
+              exact 
               path={objRoute.url}
               key={i}
               element={<objRoute.component_name />}
