b4679d86dd99f28ca41d0bebe38593cf6d4b6ca2
Tejas Thorat <tejasthorat7777@gmail.com>
2024-06-29 20:49:06 +0530

    added toast and css for it , changes in home page added new products

--- src/Componet/pages/Home.tsx
+++ src/Componet/pages/Home.tsx
@@ -9,61 +9,105 @@ import {
 } from "@mui/material";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
-import harnessCard from "../../assets/harness_copy.png"
+import AddIcon from "@mui/icons-material/Add";
+import harnessCard from "../../assets/harness_copy.png";
+import collar from "../../assets/red-dog-collar.jpg";
+import scooper from "../../assets/poop-scooper2.png";
+import adultPedegree from "../../assets/adultPedegree.jpg";
+import tickShampoo from "../../assets/tickfree_shampoo_200ml_1.jpg";
+import leash from "../../assets/leashHome.jpg";
+import purinaDry from "../../assets/purinaDryFood.jpg";
+import k9Harness from "../../assets/K9Harness.jpg";
+import { flexDiv } from "../../commonFiles/commonTheme";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import styles from "../../commonFiles/toast.module.css";
 
 const outerDiv = {
   height: "100%",
   width: "100%",
   backgroundColor: "#597081",
-  padding: "3%",
+  padding: "1.5rem",
 };
 
 const commonDiv = {
   display: "flex",
-  height: "50%",
+  height: "18rem",
   justifyContent: "space-evenly",
 };
 
+const cardContent = {
+  backgroundColor: "#00111c",
+  color: "white",
+  ...flexDiv,
+  justifyContent: "space-between",
+};
+
+const cardMedia = {
+  height: "100%",
+  objectFit: "cover",
+  ...flexDiv,
+};
 const Home = () => {
+  const handleToaste = () =>
+    toast("Item added to Cart", {
+      autoClose: 1000,
+    });
 
   const firstRowcard = [
-    { cardText: "Harness", cardImage: harnessCard },
-    { cardText: "Harness", cardImage: harnessCard },
-    { cardText: "Harness", cardImage: harnessCard },
-    { cardText: "Harness", cardImage: harnessCard },
+    { cardText: "Harness", cardImage: harnessCard, cardPrice: "759" },
+    { cardText: "Collar", cardImage: collar, cardPrice: "259" },
+    { cardText: "Poo Scooper", cardImage: scooper, cardPrice: "389" },
+    { cardText: "Pendent", cardImage: adultPedegree, cardPrice: "699" },
   ];
   const secondRowcard = [
-    { cardText: "Harness", cardImage: harnessCard },
-    { cardText: "Harness", cardImage: harnessCard },
-    { cardText: "Harness", cardImage: harnessCard },
-    { cardText: "Harness", cardImage: harnessCard },
+    { cardText: "Harness", cardImage: tickShampoo, cardPrice: "759" },
+    { cardText: "Harness", cardImage: purinaDry, cardPrice: "389" },
+    { cardText: "Harness", cardImage: k9Harness, cardPrice: "699" },
+    { cardText: "Harness", cardImage: leash, cardPrice: "259" },
   ];
 
   const buttonIcon = [
     <ShoppingCartOutlinedIcon />,
     <FavoriteBorderOutlinedIcon />,
-    <ShareOutlinedIcon />,
+    <AddIcon />,
   ];
 
   return (
     <div style={outerDiv}>
       <div style={commonDiv}>
         {firstRowcard.map((cardObject, index) => (
-          <Card sx={{ maxWidth: 220 }}>
-            <CardActionArea key={index}>
-              <CardMedia
-                sx={{ height: 185 }}
-                image={cardObject.cardImage}
-                title={cardObject.cardText}
-              />
+          <Card sx={{ maxWidth: "15rem" }}>
+            <CardActionArea
+              key={index}
+              style={{ height: "11.5rem", padding: 10 }}
+            >
+              <CardMedia sx={cardMedia} title={cardObject.cardText}>
+                <img
+                  src={cardObject.cardImage}
+                  style={{ height: "100%", width: "auto" }}
+                />
+              </CardMedia>
             </CardActionArea>
-            <CardContent sx={{ backgroundColor: "#00111c", color: "white" }}>
+            <CardContent sx={cardContent}>
               <Typography>{cardObject.cardText}</Typography>
+              <Typography>{`₹ ${cardObject.cardPrice}.00`}</Typography>
             </CardContent>
             <CardActions sx={{ backgroundColor: "#00111c" }}>
               {buttonIcon.map((iconName, index) => (
-                <Button size="small" key={index} style={{ color: "white" }}>
+                <Button
+                  size="small"
+                  key={index}
+                  style={{ color: "white" }}
+                  onClick={handleToaste}
+                >
+                  <ToastContainer
+                    position="bottom-left"
+                    toastClassName={styles.toast}
+                    bodyClassName={styles.body}
+                    hideProgressBar={true}
+                    autoClose={1000}
+                  />
                   {iconName}
                 </Button>
               ))}
@@ -74,20 +118,32 @@ const Home = () => {
       <div
         style={{
           ...commonDiv,
-          marginTop: "2%",
+          marginTop: "1rem",
         }}
       >
         {secondRowcard.map((cardObject, index) => (
           <Card sx={{ maxWidth: 220 }}>
-            <CardActionArea key={index}>
+            <CardActionArea
+              key={index}
+              style={{ height: "11.5rem", padding: 10 }}
+            >
               <CardMedia
-                sx={{ height: 185 }}
-                image={cardObject.cardImage}
-                title="Harness"
-              />
+                style={{
+                  height: "100%",
+                  objectFit: "cover",
+                  ...flexDiv,
+                }}
+                title={cardObject.cardText}
+              >
+                <img
+                  src={cardObject.cardImage}
+                  style={{ height: "100%", width: "auto" }}
+                />
+              </CardMedia>
             </CardActionArea>
-            <CardContent sx={{ backgroundColor: "#00111c", color: "white" }}>
+            <CardContent sx={cardContent}>
               <Typography>{cardObject.cardText}</Typography>
+              <Typography>{`₹ ${cardObject.cardPrice}.00`}</Typography>
             </CardContent>
             <CardActions sx={{ backgroundColor: "#00111c" }}>
               {buttonIcon.map((iconName, index) => (
