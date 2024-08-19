f9f725e2773ea8b9095c0350bb5a2712db36b2ed
Tejas Thorat <tejasthorat7777@gmail.com>
2024-08-19 16:11:23 +0530

    text will not be accept by price discount weight and quant

--- src/business/component/AddProduct.tsx
+++ src/business/component/AddProduct.tsx
@@ -99,225 +99,244 @@ const AddProduct = () => {
   };
 
   return (
-    <div style={homeStyle.outerDiv}>
+    <>
       {isLoading ? (
         <CircularProgress />
       ) : (
-        <div style={businessAddproduct.outerDiv}>
-          <div
-            style={{
-              height: "100%",
-              width: "40%",
-            }}
-          >
-            <div style={businessAddproduct.imgOuter}>
-              {!addProduct.prodImg ? (
-                <div style={businessAddproduct.uploadBtnDiv}>
-                  <Button
-                    style={businessAddproduct.uploadBtn}
-                    component="label"
-                    variant="outlined"
-                    tabIndex={-1}
-                    startIcon={<CloudUploadIcon />}
-                  >
-                    Upload Image
-                    <VisuallyHiddenInput
-                      type="file"
-                      id="prodFile"
-                      onChange={handleImageUpload}
-                    />
-                  </Button>
-                </div>
-              ) : (
-                <Card data-testid={`product_`}>
-                  <CardActionArea sx={{ height: "17rem", ...flexDiv }}>
-                    <CardMedia sx={{ height: "14rem", objectFit: "cover" }}>
-                      <img
-                        src={addProduct.prodImg}
-                        style={cartStyle.imageStyle}
-                      />
-                    </CardMedia>
-                  </CardActionArea>
-                </Card>
-              )}
-            </div>
-            <input
-              type="text"
-              name="Brand"
-              data-testid="prodBrand"
-              placeholder="Product Brand"
-              autoComplete="off"
-              value={addProduct.prodBrand}
-              style={businessAddproduct.productBrand}
-              onChange={(event) => {
-                setAddProduct({
-                  ...addProduct,
-                  prodBrand: event.target.value,
-                  prodId: generateProductId(12),
-                });
+        <div style={homeStyle.outerDiv}>
+          <div style={businessAddproduct.outerDiv}>
+            <div
+              style={{
+                height: "100%",
+                width: "40%",
               }}
-            />
-            <FormControl
-              data-testid="selectForm"
-              sx={{
-                width: "50%",
-              }}
-              size="small"
-              style={{ marginTop: "5%", color: "black" }}
             >
-              <Select
-                data-testid="prodCondition"
-                sx={{
-                  "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                    border: "none",
-                  },
-                  fontFamily: "cursive",
-                  color:
-                    addProduct.prodConditon === "Condition" ? "grey" : "black",
-                }}
-                value={addProduct.prodConditon}
-                style={{
-                  backgroundColor: "white",
-                }}
+              <div style={businessAddproduct.imgOuter}>
+                {!addProduct.prodImg ? (
+                  <div style={businessAddproduct.uploadBtnDiv}>
+                    <Button
+                      style={businessAddproduct.uploadBtn}
+                      component="label"
+                      variant="outlined"
+                      tabIndex={-1}
+                      startIcon={<CloudUploadIcon />}
+                    >
+                      Upload Image
+                      <VisuallyHiddenInput
+                        type="file"
+                        id="prodFile"
+                        onChange={handleImageUpload}
+                      />
+                    </Button>
+                  </div>
+                ) : (
+                  <Card data-testid={`product_`}>
+                    <CardActionArea sx={{ height: "17rem", ...flexDiv }}>
+                      <CardMedia sx={{ height: "14rem", objectFit: "cover" }}>
+                        <img
+                          src={addProduct.prodImg}
+                          style={cartStyle.imageStyle}
+                        />
+                      </CardMedia>
+                    </CardActionArea>
+                  </Card>
+                )}
+              </div>
+              <input
+                type="text"
+                name="Brand"
+                data-testid="prodBrand"
+                placeholder="Product Brand"
+                autoComplete="off"
+                value={addProduct.prodBrand}
+                style={businessAddproduct.productBrand}
                 onChange={(event) => {
                   setAddProduct({
                     ...addProduct,
-                    prodConditon: event.target.value as string,
+                    prodBrand: event.target.value,
+                    prodId: generateProductId(12),
                   });
                 }}
+              />
+              <FormControl
+                data-testid="selectForm"
+                sx={{
+                  width: "50%",
+                }}
+                size="small"
+                style={{ marginTop: "5%", color: "black" }}
               >
-                {condition.map((condition, index) => (
-                  <MenuItem
-                    sx={{ fontFamily: "cursive" }}
-                    value={condition}
-                    key={`condition_${condition}_${index}`}
-                    data-testid={`condition_${condition}`}
-                  >
-                    {condition}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <SendButton
-              operationOnData={reset}
-              text={"Cancel"}
+                <Select
+                  data-testid="prodCondition"
+                  sx={{
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      border: "none",
+                    },
+                    fontFamily: "cursive",
+                    color:
+                      addProduct.prodConditon === "Condition"
+                        ? "grey"
+                        : "black",
+                  }}
+                  value={addProduct.prodConditon}
+                  style={{
+                    backgroundColor: "white",
+                  }}
+                  onChange={(event) => {
+                    setAddProduct({
+                      ...addProduct,
+                      prodConditon: event.target.value as string,
+                    });
+                  }}
+                >
+                  {condition.map((condition, index) => (
+                    <MenuItem
+                      sx={{ fontFamily: "cursive" }}
+                      value={condition}
+                      key={`condition_${condition}_${index}`}
+                      data-testid={`condition_${condition}`}
+                    >
+                      {condition}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <SendButton
+                operationOnData={reset}
+                text={"Cancel"}
+                style={{
+                  top: "70%",
+                  left: "10%",
+                  backgroundColor: "#597081",
+                }}
+              />
+              <SendButton
+                operationOnData={dumpProduct}
+                text={"Submit"}
+                style={{
+                  top: "70%",
+                  left: "25%",
+                  backgroundColor: "#597081",
+                }}
+              />
+            </div>
+            <Container
               style={{
-                top: "70%",
-                left: "10%",
-                backgroundColor: "#597081",
+                width: "60%",
               }}
-            />
-            <SendButton
-              operationOnData={dumpProduct}
-              text={"Submit"}
-              style={{
-                top: "70%",
-                left: "25%",
-                backgroundColor: "#597081",
-              }}
-            />
+            >
+              <input
+                type="text"
+                name="prodName"
+                data-testid="prodName"
+                placeholder="Product Name"
+                autoComplete="off"
+                value={addProduct.prodName}
+                style={businessAddproduct.prodName}
+                onChange={(event) => {
+                  setAddProduct({
+                    ...addProduct,
+                    prodName: event.target.value,
+                  });
+                }}
+              />
+              <textarea
+                name="prodDiscription"
+                data-testid="prodDiscription"
+                placeholder="Description of Product"
+                autoComplete="off"
+                value={addProduct.prodDiscrip}
+                style={businessAddproduct.prodDiscrip}
+                onChange={(event) => {
+                  setAddProduct({
+                    ...addProduct,
+                    prodDiscrip: event.target.value,
+                  });
+                }}
+              />
+              <input
+                type="text"
+                name="price"
+                data-testid="prodPrice"
+                placeholder="Product Price (M.R.P.)"
+                autoComplete="off"
+                value={addProduct.prodPrice}
+                style={businessAddproduct.prodPriceQuantWeight}
+                onChange={(event) => {
+                  const value = event.target.value;
+                  if (/^\d*\.?\d*$/.test(value)) {
+                    setAddProduct({
+                      ...addProduct,
+                      prodPrice: value,
+                    });
+                  }
+                }}
+              />
+              <input
+                type="text"
+                name="Quant"
+                data-testid="prodQuant"
+                placeholder="Net Quantity"
+                autoComplete="off"
+                value={addProduct.pordQuant}
+                style={{
+                  ...businessAddproduct.prodPriceQuantWeight,
+                  marginLeft: "10%",
+                }}
+                onChange={(event) => {
+                  const value = event.target.value;
+                  if (/^\d*\.?\d*$/.test(value)) {
+                    setAddProduct({
+                      ...addProduct,
+                      pordQuant: value,
+                    });
+                  }
+                }}
+              />
+              <input
+                type="text"
+                name="weight"
+                data-testid="prodWeight"
+                placeholder="Product Weight"
+                autoComplete="off"
+                value={addProduct.prodWeight}
+                style={businessAddproduct.prodPriceQuantWeight}
+                onChange={(event) => {
+                  const value = event.target.value;
+                  if (/^\d*\.?\d*$/.test(value)) {
+                    setAddProduct({
+                      ...addProduct,
+                      prodWeight: value,
+                    });
+                  }
+                }}
+              />
+              <input
+                type="text"
+                name="discount"
+                data-testid="prodDiscont"
+                placeholder="Discount in %"
+                autoComplete="off"
+                value={addProduct.prodDiscount}
+                style={{
+                  ...businessAddproduct.prodPriceQuantWeight,
+                  marginLeft: "10%",
+                }}
+                onChange={(event) => {
+                  const value = event.target.value;
+                  if (/^\d*\.?\d*$/.test(value)) {
+                    setAddProduct({
+                      ...addProduct,
+                      prodDiscount: value,
+                    });
+                  }
+                }}
+              />
+            </Container>
           </div>
-          <Container
-            style={{
-              width: "60%",
-            }}
-          >
-            <input
-              type="text"
-              name="prodName"
-              data-testid="prodName"
-              placeholder="Product Name"
-              autoComplete="off"
-              value={addProduct.prodName}
-              style={businessAddproduct.prodName}
-              onChange={(event) => {
-                setAddProduct({ ...addProduct, prodName: event.target.value });
-              }}
-            />
-            <textarea
-              name="prodDiscription"
-              data-testid="prodDiscription"
-              placeholder="Description of Product"
-              autoComplete="off"
-              value={addProduct.prodDiscrip}
-              style={businessAddproduct.prodDiscrip}
-              onChange={(event) => {
-                setAddProduct({
-                  ...addProduct,
-                  prodDiscrip: event.target.value,
-                });
-              }}
-            />
-            <input
-              type="text"
-              name="price"
-              data-testid="prodPrice"
-              placeholder="Product Price (M.R.P.)"
-              autoComplete="off"
-              value={addProduct.prodPrice}
-              style={businessAddproduct.prodPriceQuantWeight}
-              onChange={(event) => {
-                setAddProduct({
-                  ...addProduct,
-                  prodPrice: event.target.value,
-                });
-              }}
-            />
-            <input
-              type="text"
-              name="Quant"
-              data-testid="prodQuant"
-              placeholder="Net Quantity"
-              autoComplete="off"
-              value={addProduct.pordQuant}
-              style={{
-                ...businessAddproduct.prodPriceQuantWeight,
-                marginLeft: "10%",
-              }}
-              onChange={(event) => {
-                setAddProduct({
-                  ...addProduct,
-                  pordQuant: event.target.value,
-                });
-              }}
-            />
-            <input
-              type="text"
-              name="weight"
-              data-testid="prodWeight"
-              placeholder="Product Weight"
-              autoComplete="off"
-              value={addProduct.prodWeight}
-              style={businessAddproduct.prodPriceQuantWeight}
-              onChange={(event) => {
-                setAddProduct({
-                  ...addProduct,
-                  prodWeight: event.target.value,
-                });
-              }}
-            />
-            <input
-              type="text"
-              name="discount"
-              data-testid="prodDiscont"
-              placeholder="Discount in %"
-              autoComplete="off"
-              value={addProduct.prodDiscount}
-              style={{
-                ...businessAddproduct.prodPriceQuantWeight,
-                marginLeft: "10%",
-              }}
-              onChange={(event) => {
-                setAddProduct({
-                  ...addProduct,
-                  prodDiscount: event.target.value,
-                });
-              }}
-            />
-          </Container>
         </div>
       )}
-    </div>
+    </>
   );
 };
 export default AddProduct;
