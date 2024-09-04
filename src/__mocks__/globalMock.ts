7fa0bd11ba87123861192740528956f82dbbb730
Tejas Thorat <tejasthorat7777@gmail.com>
2024-09-04 23:58:23 +0530

    home test case corrected

--- src/__mocks__/globalMock.ts
+++ src/__mocks__/globalMock.ts
@@ -4,14 +4,24 @@ import { vi } from "vitest";
 export const mockAxiosGet = vi.fn().mockImplementation(() => {});
 export const mockAxiosPost = vi.fn().mockImplementation(() => {});
 export const mockShare = vi.fn().mockImplementation(() => {});
-export const mockConsoleLog = vi.fn().mockImplementation(() => {});
-export const mockConsoleError = vi.fn().mockImplementation(() => {});
-
 
 vi.spyOn(axios, "get").mockImplementation(mockAxiosGet);
 vi.spyOn(axios, "post").mockImplementation(mockAxiosPost);
-vi.spyOn(console,"log").mockImplementation(mockConsoleLog);
-vi.spyOn(console,"error").mockImplementation(mockConsoleError);
+
+function rgbToHex(r: any, g: any, b: any) {
+  const redHex = r.toString(16).padStart(2, "0");
+  const greenHex = g.toString(16).padStart(2, "0");
+  const blueHex = b.toString(16).padStart(2, "0");
+  return `#${redHex}${greenHex}${blueHex}`;
+}
+export const rgbStringToHex = (rgbString: any) => {
+  const result = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/.exec(rgbString);
+  if (!result) {
+    throw new Error("Invalid RGB string");
+  }
+  const [, r, g, b] = result;
+  return rgbToHex(parseInt(r, 10), parseInt(g, 10), parseInt(b, 10));
+};
 
 Object.assign(navigator, {
   share: mockShare,
--- src/__test__/Home.test.tsx
+++ src/__test__/Home.test.tsx
@@ -1,17 +1,20 @@
 import Wrapper from "../setupTest/Wrapper";
 import Home from "../Componet/pages/Home";
+import { fireEvent, render, screen, act } from "@testing-library/react";
 import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-  act,
-} from "@testing-library/react";
-import {
+  mockAxiosGet,
   mockAxiosPost,
-  mockConsoleLog,
   mockShare,
+  rgbStringToHex,
 } from "../__mocks__/globalMock";
+
+vi.mock("react-lottie-player", () => {
+  return {
+    default: vi.fn(() => {
+      return <div data-testid="loginlottie"></div>;
+    }),
+  };
+});
 
 const mockUser = {
   name: "",
@@ -23,12 +26,72 @@ const mockUser = {
   username: "",
   userId: "",
   gender: "",
-  city: "",
+  acc_type: "",
   password: "",
 };
 
+const dummpProducts = [];
+
+for (let i = 1; i <= 16; i++) {
+  const result = {
+    prodId: `${i}`,
+    prodName: "Pedegree",
+    prodDiscrip: "nutrients",
+    prodPrice: "100",
+    pordQuant: "1",
+    prodDiscount: "2",
+    prodBrand: "Balaji",
+    prodWeight: "100",
+    prodConditon: "new",
+    prodImg: "",
+    selected: false,
+    rating: 0,
+    category: "Dog Food",
+    subCategory: "Treat",
+  };
+  dummpProducts.push(result);
+}
+
+const productsOut = [
+  {
+    products: dummpProducts,
+  },
+];
+
+const items = [
+  {
+    prodDiscrip: "Unitest State of America",
+    prodId: "Od3nbwBFilvi",
+    prodImg: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAgA",
+    prodName: "american cheese",
+    prodPrice: "123",
+    selected: false,
+  },
+];
+
 describe("Home", () => {
-  it("TC:1 should change icon when user click on add to wishlist button", async () => {
+  vi.useFakeTimers();
+  beforeEach(() => {
+    mockAxiosGet
+      .mockImplementationOnce(async () => {
+        return {
+          data: productsOut,
+        };
+      })
+      .mockImplementation(async () => {
+        return {
+          data: {
+            customerId: "936819838169",
+            items: items,
+          },
+        };
+      });
+  });
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.clearAllTimers();
+  });
+  it("TC:1 should change icon when user clicks on add to wishlist button", async () => {
     const mockInitialState = {
       status: false,
       user: { ...mockUser, userId: "123456" },
@@ -38,21 +101,27 @@ describe("Home", () => {
         <Home />
       </Wrapper>
     );
-    expect(
-      screen.getByTestId("FavoriteBorderOutlinedIcon_5")
-    ).toBeInTheDocument();
-    expect(
-      // here {} indicates that it has style as per MUI we have not given any style from our side
-      screen.getByTestId("FavoriteBorderOutlinedIcon_5").style._values
-    ).toEqual({});
+
+    const FavoriteBorderOutlinedIcon = `FavoriteBorderOutlinedIcon_${productsOut[0].products[0].prodId}`;
+    const FavoriteIcon = `FavoriteIcon_${productsOut[0].products[0].prodId}`;
+    const wishilistBtn = `wishlist_${productsOut[0].products[0].prodId}`;
 
     await act(async () => {
-      fireEvent.click(screen.getByTestId("wishlist_5"));
+      vi.advanceTimersByTime(5000);
+    });
+    expect(screen.getByTestId(FavoriteBorderOutlinedIcon)).toBeInTheDocument();
+    await act(async () => {
+      fireEvent.click(screen.getByTestId(wishilistBtn));
     });
 
-    expect(screen.getByTestId("FavoriteIcon_5")).toBeInTheDocument();
-    expect(screen.getByTestId("FavoriteIcon_5").style._values).toEqual({
-      color: "red",
+    expect(screen.getByTestId(FavoriteIcon)).toBeInTheDocument();
+
+    // checking icon color after clicked
+    const favoriteIconElement = screen.getByTestId(FavoriteIcon);
+    const computedStyle = window.getComputedStyle(favoriteIconElement);
+    const backClr = rgbStringToHex(computedStyle.color);
+    expect(screen.getByTestId(FavoriteIcon)).toHaveStyle({
+      color: backClr,
     });
   });
 
@@ -62,6 +131,9 @@ describe("Home", () => {
         <Home />
       </Wrapper>
     );
+    await act(async () => {
+      vi.advanceTimersByTime(5000);
+    });
     expect(screen.getByTestId("product_1")).toBeInTheDocument();
     expect(screen.getByTestId("product_2")).toBeInTheDocument();
     expect(screen.getByTestId("product_3")).toBeInTheDocument();
@@ -86,26 +158,32 @@ describe("Home", () => {
         <Home />
       </Wrapper>
     );
-    const paginationButton = screen.getByLabelText("Go to next page");
-    fireEvent.click(paginationButton);
-    await waitFor(() => {
-      expect(screen.queryByTestId("product_1")).not.toBeInTheDocument();
-      expect(screen.queryByTestId("product_2")).not.toBeInTheDocument();
-      expect(screen.queryByTestId("product_3")).not.toBeInTheDocument();
-      expect(screen.queryByTestId("product_4")).not.toBeInTheDocument();
-      expect(screen.queryByTestId("product_5")).not.toBeInTheDocument();
-      expect(screen.queryByTestId("product_6")).not.toBeInTheDocument();
-      expect(screen.queryByTestId("product_7")).not.toBeInTheDocument();
-      expect(screen.queryByTestId("product_8")).not.toBeInTheDocument();
-      expect(screen.queryByTestId("product_9")).toBeInTheDocument();
-      expect(screen.queryByTestId("product_10")).toBeInTheDocument();
-      expect(screen.queryByTestId("product_11")).toBeInTheDocument();
-      expect(screen.queryByTestId("product_12")).toBeInTheDocument();
-      expect(screen.queryByTestId("product_13")).toBeInTheDocument();
-      expect(screen.queryByTestId("product_14")).toBeInTheDocument();
-      expect(screen.queryByTestId("product_15")).toBeInTheDocument();
-      expect(screen.queryByTestId("product_16")).toBeInTheDocument();
+    await act(async () => {
+      vi.advanceTimersByTime(5000);
     });
+    await act(async () => {
+      vi.advanceTimersByTime(5000);
+    });
+    const paginationButton = screen.getByLabelText("Go to next page");
+    await act(async () => {
+      fireEvent.click(paginationButton);
+    });
+    expect(screen.queryByTestId("product_1")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("product_2")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("product_3")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("product_4")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("product_5")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("product_6")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("product_7")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("product_8")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("product_9")).toBeInTheDocument();
+    expect(screen.queryByTestId("product_10")).toBeInTheDocument();
+    expect(screen.queryByTestId("product_11")).toBeInTheDocument();
+    expect(screen.queryByTestId("product_12")).toBeInTheDocument();
+    expect(screen.queryByTestId("product_13")).toBeInTheDocument();
+    expect(screen.queryByTestId("product_14")).toBeInTheDocument();
+    expect(screen.queryByTestId("product_15")).toBeInTheDocument();
+    expect(screen.queryByTestId("product_16")).toBeInTheDocument();
   });
 
   it("TC:4 should display toast message, when user is not login and try to add product to wishlist", async () => {
@@ -115,11 +193,16 @@ describe("Home", () => {
       </Wrapper>
     );
 
+    const wishilistBtn = `wishlist_${productsOut[0].products[0].prodId}`;
     await act(async () => {
-      fireEvent.click(screen.getByTestId("wishlist_5"));
+      vi.advanceTimersByTime(5000);
     });
-
-    expect(screen.getByText("Please Login...")).toBeInTheDocument();
+    await act(async () => {
+      fireEvent.click(screen.getByTestId(wishilistBtn));
+    });
+    expect(
+      screen.getByText("Please log in to add items to your wishlist.")
+    ).toBeInTheDocument();
   });
 
   it("TC:5 Product should add to wishlist, when user is logged in", async () => {
@@ -132,21 +215,33 @@ describe("Home", () => {
         <Home />
       </Wrapper>
     );
+    const wishilistBtn = `wishlist_${productsOut[0].products[0].prodId}`;
     await act(async () => {
-      fireEvent.click(screen.getByTestId("wishlist_5"));
+      vi.advanceTimersByTime(5000);
+    });
+    await act(async () => {
+      fireEvent.click(screen.getByTestId(wishilistBtn));
     });
 
     expect(screen.getByText("Item added to Wishlist")).toBeInTheDocument();
     expect(mockAxiosPost).toHaveBeenLastCalledWith(
       "http://localhost:3000/wishlist/dumped",
       {
+        prodId: "1",
+        category: "Dog Food",
         customerId: "123456",
-        productId: "5",
-        prouctName: "Tickfree",
-        imageSource: "/src/assets/tickfree_shampoo_200ml_1.jpg",
-        price: "759",
+        prodName: "Pedegree",
+        prodDiscrip: "nutrients",
+        prodPrice: "100",
+        pordQuant: "1",
+        prodDiscount: "2",
+        prodBrand: "Balaji",
+        prodWeight: "100",
+        prodConditon: "new",
+        prodImg: "",
         selected: false,
-        description: "",
+        rating: 0,
+        subCategory: "Treat",
       }
     );
   });
@@ -161,17 +256,21 @@ describe("Home", () => {
         <Home />
       </Wrapper>
     );
+    const wishilistBtn = `wishlist_${productsOut[0].products[0].prodId}`;
     await act(async () => {
-      fireEvent.click(screen.getByTestId("wishlist_5"));
+      vi.advanceTimersByTime(5000);
+    });
+    await act(async () => {
+      fireEvent.click(screen.getByTestId(wishilistBtn));
     });
     expect(screen.getByText("Item added to Wishlist")).toBeInTheDocument();
     await act(async () => {
-      fireEvent.click(screen.getByTestId("wishlist_5"));
+      fireEvent.click(screen.getByTestId(wishilistBtn));
     });
     expect(screen.getByText("Item removed from Wishlist")).toBeInTheDocument();
   });
 
-  it("TC:7 should display unable to add in wishlist, when axios reject dumping of data", async () => {
+  it("TC:7 should display  Error updating wishlist. Please try again later., when axios reject dumping of data", async () => {
     mockAxiosPost.mockRejectedValue({});
     const mockInitialState = {
       status: false,
@@ -182,13 +281,19 @@ describe("Home", () => {
         <Home />
       </Wrapper>
     );
+    const wishilistBtn = `wishlist_${productsOut[0].products[0].prodId}`;
     await act(async () => {
-      fireEvent.click(screen.getByTestId("wishlist_5"));
+      vi.advanceTimersByTime(5000);
     });
-    expect(screen.getByText("Unable to add in Wishlist")).toBeInTheDocument();
+    await act(async () => {
+      fireEvent.click(screen.getByTestId(wishilistBtn));
+    });
+    expect(
+      screen.getByText("Error updating wishlist. Please try again later.")
+    ).toBeInTheDocument();
   });
 
-  it("TC:8 should log Share successful when user hit share button", async () => {
+  it("TC:8 share funtion should called when user hit share button", async () => {
     const mockInitialState = {
       status: false,
       user: { ...mockUser, userId: "123456" },
@@ -199,12 +304,15 @@ describe("Home", () => {
       </Wrapper>
     );
     await act(async () => {
-      fireEvent.click(screen.getByTestId("share_5"));
+      vi.advanceTimersByTime(5000);
     });
-    expect(mockConsoleLog).toHaveBeenCalledWith("Share successful");
+    await act(async () => {
+      fireEvent.click(screen.getByTestId("share_1"));
+    });
+    expect(mockShare).toHaveBeenCalled();
   });
 
-  it("TC:9 should log Error sharing and error and should display Sorry, Error in sharing, when user hit share button and error occured", async () => {
+  it("TC:9 should display Sorry, Error in sharing, when user hit share button and error occured", async () => {
     mockShare.mockRejectedValue({});
     const mockInitialState = {
       status: false,
@@ -216,9 +324,11 @@ describe("Home", () => {
       </Wrapper>
     );
     await act(async () => {
-      fireEvent.click(screen.getByTestId("share_5"));
+      vi.advanceTimersByTime(5000);
     });
-    expect(mockConsoleLog).toHaveBeenCalledWith("Error sharing", {});
+    await act(async () => {
+      fireEvent.click(screen.getByTestId("share_1"));
+    });
     expect(screen.getByText("Sorry, Error in sharing")).toBeInTheDocument();
   });
 });
